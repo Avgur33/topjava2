536c9768f35c689b49bb956f66f6e0984679ad92
Avgur33 <avgur33@gmail.com>
2021-08-24 23:31:49 +0300

    refactoring dateUtil

--- src/main/java/ru/javaops/topjava2/util/DateUtil.java
+++ src/main/java/ru/javaops/topjava2/util/DateUtil.java
@@ -1,17 +1,11 @@
 package ru.javaops.topjava2.util;
 
-import org.springframework.lang.Nullable;
-import org.springframework.util.StringUtils;
-
 import java.time.LocalDate;
-import java.time.LocalTime;
 
 public class DateUtil {
 
     public static final LocalDate DATE_MIN = LocalDate.of(2000,1,1);
     public static final LocalDate DATE_MAX = LocalDate.of(3000,1,1);
-
-    //public static final LocalTime TIME_LIMIT = LocalTime.of(11,00);
 
     public static LocalDate startDateUtil(LocalDate startDate){
         if ((startDate == null) || (startDate.compareTo(DATE_MIN)<=0)){
@@ -26,9 +20,5 @@ public class DateUtil {
         }else {
             return endDate;
         }
-    }
-    public static @Nullable
-    LocalTime parseLocalTime(@Nullable String str) {
-        return StringUtils.hasLength(str) ? LocalTime.parse(str) : null;
     }
 }
